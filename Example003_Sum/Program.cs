f6328eefaca8c78272370c44f4782ba9c8b0ccd9
Galprim <galprimulus@gmail.com>
2022-04-24 23:17:27 +0300

    add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numperA = 32;
-int numberB = 53;
-int result = numperA + numberB;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
